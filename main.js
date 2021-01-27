9ffc572a70524e0db2c9685b3865a1604c470605
Antonin Rousseau <34874048+toxicbloud@users.noreply.github.com>
2021-01-27 15:03:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+
 function playclip(){
   var audio = document.getElementsByTagName("audio")[0];
            audio.play();
